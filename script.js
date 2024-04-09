b2450587a5c9d3d7842ca8ed3e922a8fd68f9209
viveoff <vlad.shorodok14@gmail.com>
2024-04-09 09:11:57 +0300

    q

---
+++ script.js
@@ -0,0 +1,27 @@
+const rockButton = document.getElementById('rock');
+const paperButton = document.getElementById('paper')
+const scissorsButton = document.getElementById('scissors')
+const displayResult = document.getElementById('result')
+
+
+rockButton.addEventListener('click', () => playRound('rock'));
+paperButton.addEventListener('click', () => playRound('paper'));
+scissorsButton.addEventListener('click', () => playRound('scissors'));
+
+
+function playRound(playerChoice) {
+    const choices = ['rock', 'paper', 'scissors'];
+    const computerChoice = choices[Math.floor(Math.random()*choices.length)];
+
+    if (playerChoice === computerChoice) {
+        displayResult.textContent = "It's a draw."
+    } else if (
+        (playerChoice === 'rock' && computerChoice === 'scissors') ||
+        (playerChoice === 'paper' && computerChoice === 'rock') ||
+        (playerChoice === 'scissors' && computerChoice === 'paper')
+    ) {
+        displayResult.textContent = 'You win!';
+    } else {
+        displayResult.textContent = 'Computer wins!';
+    }
+}
